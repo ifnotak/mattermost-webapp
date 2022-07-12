3d6284a9e0490e63ebf4d7dba7f87b7a4b4c4e84
Ashish Dhama <ashishdhama3@gmail.com>
2022-07-12 10:26:23 +0530

    add new channels setting modal

---
+++ components/new_user_settings/notifications/constants.ts
@@ -0,0 +1,11 @@
+export const NOTIFICATION_CONSTANTS = {
+    ALL_NEW_MESSAGES: "All new messages",
+    MENTION: "Mentions, direct messages, and keywords only",
+    NONE: "Nothing",
+    ONLINE: "Online, away or offline",
+    AWAY: "Away or offline",
+    OFFLINE: "Offline",
+    IMMEDIATELY: "Immediately",
+    FIFTEEN_MINUTES: "Once every 15 minutes",
+    EVERY_HOUR: "Once every hour",
+};
